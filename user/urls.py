77ee085e92b0c32c3f437269557f50cbeadcbbd1
aqilfiqran <gamingaft@gmail.com>
2020-09-02 12:04:16 +0700

    block templating

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import indexView
+from .views import indexView, aboutView
 
 urlpatterns = [
-    path('', indexView, name='index')  # localhost:8000/
+    path('', indexView, name='index'),  # localhost:8000/
+    path('about', aboutView, name='about'),  # localhost:8000/
 ]
--- user/views.py
+++ user/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def indexView(request):
     return render(request, 'index.html')
+
+
+def aboutView(request):
+    return render(request, 'about.html')
