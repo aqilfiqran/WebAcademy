e640218f9a2689b490f6e93f2b71355771106132
aqilfiqran <gamingaft@gmail.com>
2020-09-02 12:12:26 +0700

    class based view

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
 from .views import indexView, aboutView
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path('', indexView, name='index'),  # localhost:8000/
-    path('about', aboutView, name='about'),  # localhost:8000/
+    path('about', aboutView, name='about'),  # localhost:8000/about
+    path('kelas', TemplateView.as_view(template_name="kelas.html"),
+         name='kelas'),  # localhost:8000/kelas
 ]
