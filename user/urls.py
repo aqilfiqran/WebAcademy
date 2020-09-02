18abb1274275b4d81d07fefa82f427a774344bd9
aqilfiqran <gamingaft@gmail.com>
2020-09-02 12:21:04 +0700

    request.path

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from .views import indexView, aboutView
 from django.views.generic import TemplateView
+
+app_name = "user"
 urlpatterns = [
     path('', indexView, name='index'),  # localhost:8000/
     path('about', aboutView, name='about'),  # localhost:8000/about
