6108be5c540908bae6d32f91df79d97675d5f02e
aqilfiqran <gamingaft@gmail.com>
2020-09-02 10:23:39 +0700

    polimorfisme

--- fungsi.py
+++ fungsi.py
@@ -17,3 +17,25 @@ def panggilNama(nama='unknown', umur=0):
 # print(sayHello())
 
 panggilNama("Aqil", 20)
+
+
+def kasihNilai():
+    return 20
+
+
+# saat memanggil kasihNilai() == 20
+nama = kasihNilai()
+# nama = 20
+
+print(nama)
+
+
+def mahasiswa(nama='unknown', umur=20):
+    print(f'nama: {nama}\numur : {umur}')
+
+
+# method == fungsi
+# polimorfisme
+mahasiswa()
+mahasiswa('Aqil')
+mahasiswa('Aqil', 22)
