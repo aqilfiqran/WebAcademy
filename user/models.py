36056fce5157f073d308deffe8c9d365f8d13ba2
aqilfiqran <gamingaft@gmail.com>
2020-09-03 09:17:17 +0700

    model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.utils.translation import gettext as _
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(_("Judul"), max_length=100)
+    description = models.TextField(_("Deskripsi"))
+    image = models.ImageField(_("Gambar"), upload_to='article/')
+    slug = models.SlugField(_("Slug"), editable=False)
+    created_at = models.DateTimeField(
+        _("buat"), auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(
+        _("ubah"), auto_now=True, auto_now_add=False)
